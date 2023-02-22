430ce5da1af759f616d34d9220d1492f6cd77634
katokeita <49501166+katokeita@users.noreply.github.com>
2023-02-22 14:50:59 +0900

    Update

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,5 +1,59 @@
 // JavaScript Document
 
+// HASH
+var locationHash = window.location.hash;
+if ( locationHash.match('^#\/') ) {
+    locationHash = undefined;
+} else if ( locationHash ) {
+    window.location.hash = '';
+}
+
+// LOAD
+$('html').addClass('loadWait');
+
+// $window load timeout.
+var commonloadTimeoutTime = 3000;
+var commonloadTimer = setTimeout(
+  function(){
+    $( window ).load();
+  }, commonloadTimeoutTime
+);
+
+$( window ).one('load', function(){
+
+  clearTimeout( commonloadTimer );
+  $('html').removeClass('loadWait');
+  
+  // Anker scroll
+  if( locationHash ) {
+    if ( $( locationHash ).length ) {
+      window.location.hash = locationHash;
+      $('html, body').scrollTop( 0 );
+      setTimeout(function(){
+      
+        var $target = $( locationHash ),
+            headerHeight = $('header').outerHeight(),
+            subMenuHeight = $('#contentsMenu').outerHeight();
+        
+        var scrollPosition = 0;
+            
+        if ( $target.is('.tabContent') ) {
+          scrollPosition = $target.closest('section').offset().top - headerHeight - subMenuHeight;
+        } else {
+          scrollPosition = $target.offset().top - headerHeight - subMenuHeight;
+        }
+            
+        $('html, body').animate({ scrollTop: scrollPosition }, 300, 'swing');
+        if( $( locationHash ).is('.toggleHeading') ) {
+          setTimeout(function(){
+            $( locationHash ).click();
+          }, 400 );
+        }
+      }, 100 );
+    }
+  }
+});
+
 (function(){
 
 /*
